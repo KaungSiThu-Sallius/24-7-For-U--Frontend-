ba16f1ad0de8718bcb0cbf9130e4a2ad3f8e3673
KaungSiThu-Sallius <76986682+KaungSiThu-Sallius@users.noreply.github.com>
2021-06-30 14:57:27 +0630

    go_top_btn

--- js/script.js
+++ js/script.js
@@ -6,3 +6,18 @@ $('.nav__trigger').on('click', function (e) {
 AOS.init({
     duration: 500,
 })
+
+var btn = $('#go_top_button');
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
