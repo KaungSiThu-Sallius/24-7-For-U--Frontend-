2e3943e32bded326ee20c4f6d2027473e700656a
KaungSiThu-Sallius <76986682+KaungSiThu-Sallius@users.noreply.github.com>
2021-07-03 19:12:07 +0630

    about_us01

--- js/script.js
+++ js/script.js
@@ -35,13 +35,7 @@ window.onload = function () {
 }
 
 
-// international_ph
-const phoneInputField = document.querySelector("#phone");
-const phoneInput = window.intlTelInput(phoneInputField, {
-    preferredCountries: ["mm"],
-    utilsScript:
-        "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
-});
+
 
 
 
